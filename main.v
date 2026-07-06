7a6faaf1b8e31cc3021cdccac06e81382a5dff11
BugraBalci <balcibugra4@gmail.com>
2026-07-06 16:49:40 +0300

    Unnecessary .bak files were removed from the repository and GitIgnore was updated.

--- main.v
+++ main.v
@@ -1,6 +1,6 @@
 module main (
     input wire [8:0] SW,
-	 input wire clk,
+	 input wire clk, // clk sinyali
 	 input wire buton,
     
     // Alt modülümüz (display_section) bu kablolara dışarıdan elektrik basacak.
@@ -26,13 +26,13 @@ module main (
 
     // === 3. GÖRÜNTÜ YAKALAMA (DATA ACQUISITION) REGISTERS ===
     // Düşen kenar geldiğinde şalterlerdeki değerleri bu kalıcı hafıza odalarına kilitleyeceğiz
-    reg [4:0] kilitli_kelime;
-    reg [2:0] kilitli_ekran;
+    reg [4:0] lock_letter;
+    reg [2:0] lock_display;
 
     always @(posedge clk) begin
         if (falling_edge) begin
-            kilitli_kelime <= letter_selection; // Sinyali gördüğün an seçimi hafızaya al
-            kilitli_ekran  <= display_section;  // Sinyali gördüğün an ekranı hafızaya al
+            lock_letter <= letter_selection; // Sinyali gördüğün an seçimi hafızaya al
+            lock_display  <= display_section;  // Sinyali gördüğün an ekranı hafızaya al
         end
     end
     
@@ -43,14 +43,14 @@ module main (
     // === 3. BİRİNCİ ALT ENTEGRE: SÖZLÜK ÇİPİ ===
     // DÜZELTME: Buraya tel adı değil, 'harf_secici' modül adı yazıldı!
     letter_selection letter_motor (
-        .letter_id(kilitli_kelime),      // Şalterden gelen 5 bitlik sayıyı sözlüğe ver
+        .letter_id(lock_letter),      // Şalterden gelen 5 bitlik sayıyı sözlüğe ver
         .letter_code(connection_cable)   // Çıkan 8 bitlik ışık kodunu bizim sanal kabloya bas
     );
 
     // === 4. İKİNCİ ALT ENTEGRE: DAĞITICI ÇİP ===
     display_section distributor_motor (
-        .letter_selection(kilitli_kelime),     // Şalterden kelime durumunu dinle
-        .display_section(kilitli_ekran),   // Şalterden hedef ekranı dinle
+        .letter_selection(lock_letter),     // Şalterden kelime durumunu dinle
+        .display_section(lock_display),   // Şalterden hedef ekranı dinle
         .letter_code_input(connection_cable),  // Sözlükten gelen harf kodunu al
         
         // Dağıtıcı çipten çıkan sonuçları anakartın fiziksel bacaklarına (HEX) bağla
